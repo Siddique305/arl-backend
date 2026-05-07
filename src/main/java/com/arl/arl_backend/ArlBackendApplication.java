db6e35358f9124ae31baae5d82566ab368c1e851
Siddique305 <mohdsiddiq.305@gmail.com>
2026-05-08 01:51:03 +0530

    Initial ARL backend setup

---
+++ src/main/java/com/arl/arl_backend/ArlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arl.arl_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArlBackendApplication.class, args);
+	}
+
+}
